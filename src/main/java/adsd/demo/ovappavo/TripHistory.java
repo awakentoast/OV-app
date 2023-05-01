7c8e2bd63eeb578144cfae24a6301ddb75db9b4b
Luuk Bouma <luuk.bouma@student.hu.nl>
2023-05-01 19:02:56 +0200

    doesnt read file when it already has been read and if multiple trips have the same occurences, display all; instead of only one

--- src/main/java/adsd/demo/ovappavo/TripHistory.java
+++ src/main/java/adsd/demo/ovappavo/TripHistory.java
@@ -4,8 +4,10 @@ import java.io.*;
 import java.util.*;
 
 public final class TripHistory {
-    //instance of TripHistory that all classes can share, invoke by 'TripHistory exampleName = TripHistory.getTripHistory'
+    //instance of TripHistory that all classes can share (Singleton), invoke by 'TripHistory exampleName = TripHistory.getTripHistory'
     private static TripHistory tripHistory = null;
+    private boolean fileHasBeenRead = false;
+
     public static TripHistory getTripHistory() {
         if (tripHistory == null) {
             tripHistory = new TripHistory();
@@ -23,15 +25,19 @@ public final class TripHistory {
     private TripHistory() {
         if (!file.isFile()) {
             System.out.println("file dit not exist already");
-            try(FileOutputStream fileOutputStream = new FileOutputStream(PATH);) {
+            try(FileOutputStream ignored = new FileOutputStream(PATH)) {
                 System.out.println("file has been created");
             } catch (Exception e) {
                 e.printStackTrace();
+                System.out.println("error creating file");
             }
         } else {
             System.out.println("file already exists");
         }
-        readFileAndPutInMap();
+        if (!fileHasBeenRead) {
+            readFileAndPutInMap();
+            fileHasBeenRead = true;
+        }
     }
 
 
@@ -44,14 +50,14 @@ public final class TripHistory {
     }
 
 
-    public void readFileAndPutInMap() {
+    private void readFileAndPutInMap() {
         if (file.length() != 0) {
             StringBuilder data = new StringBuilder();
 
             try (BufferedReader br = new BufferedReader(new FileReader(file))) {
                 String line;
                 while ((line = br.readLine()) != null) {
-                    //appending \n to make splitting possible on \n, and making it nicer in the file
+                    //appending \n to make it nicer in the file, and be able top slit on the \n character
                     data.append(line).append("\n");
                 }
             } catch (Exception e) {
@@ -59,34 +65,39 @@ public final class TripHistory {
                 e.printStackTrace();
             }
 
-            String[] trips = data.toString().split("\n");
+            String[] trips = data.toString().split("\n\n");
 
             for (String trip : trips) {
+                System.out.println(trip);
                 String[] keyAndValue = trip.split("\\|");
-                tripOccurrences.put(keyAndValue[0], Integer.valueOf(keyAndValue[1]));
+                System.out.println(Arrays.toString(keyAndValue));
+                tripOccurrences.put(keyAndValue[0], Integer.valueOf(keyAndValue[1].strip()));
             }
         }
     }
 
 
-    public void getFavoriteTrip() {
+    public String getFavoriteTrip() {
         if (tripOccurrences.isEmpty()) {
-            System.out.println("you have no trip history to showcase a favorite, first travel");
+            return "you have no trip history to showcase a favorite, first travel";
         } else {
-            String favorite = "";
+            String prefix = "Favorite trip:\n ";
+            StringBuilder favorite = new StringBuilder();
             int highestOccurrence = 0;
 
             //get the trip with the most occurrences
             for (Map.Entry<String, Integer> x : tripOccurrences.entrySet()) {
                 int value = x.getValue();
                 if (value > highestOccurrence) {
-                    favorite = x.getKey();
+                    favorite = new StringBuilder(x.getKey());
                     highestOccurrence = value;
                 }
+                else if (value == highestOccurrence) {
+                    favorite.append("\n").append(x.getKey());
+                    prefix = "Favorite trips:\n";
+                }
             }
-
-            System.out.println("\nFavorite trip:");
-            System.out.println(favorite + "\n");
+            return prefix + favorite;
         }
     }
 
@@ -96,7 +107,7 @@ public final class TripHistory {
         StringBuilder data = new StringBuilder();
 
         for (Map.Entry<String, Integer> entry : tripOccurrences.entrySet()) {
-            data.append(entry.getKey()).append("|").append(entry.getValue()).append("\n");
+            data.append(entry.getKey()).append("|").append(entry.getValue()).append("\n\n");
         }
 
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
