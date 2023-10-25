238771f364ecd4b0f06bea4c751e82b13991cca4
awakentoast <luuk.bouma01@gmail.com>
2023-10-25 23:52:11 +0200

    a

--- src/main/java/adsd/demo/ovappavo/LocationFile.java
+++ src/main/java/adsd/demo/ovappavo/LocationFile.java
@@ -8,6 +8,7 @@ public class LocationFile extends FileHandler {
 
     public LocationFile(String filepath) {
         super(filepath);
+        System.out.println("a");
     }
 
     public Map<String, Location> getLocations() {
